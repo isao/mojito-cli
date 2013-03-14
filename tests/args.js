1f467f717b094d24c1d3c51af7f8305bbc19d358
Isao Yagi <isao@yahoo-inc.com>
2013-03-14 14:42:10 -0700

    suppress log output, test help error message, log stack

--- tests/args.js
+++ tests/args.js
@@ -1,26 +1,25 @@
 var test = require('tape'),
-    main = require('../');
+    cli = require('../');
 
-function noop() {}
 
-test('version', function(t) {
-    t.plan(6);
+cli.log.pause();// don't output anything
 
-    function cb(err, msg) {
-        t.equals(err, null);
-        t.ok(msg.match(/^mojito-cli v(\d+\.){2}.+/));
-    }
+function noop() {
+}
 
-    t.equals(main(['version'], cb), 'version');
-    t.equals(main(['--version'], cb), 'version');
-});
+test('missing command', function(t) {
+    t.plan(4);
 
-test('fixme --version not treated as a bool flag', function(t) {
-    // unexpected result since --version is not declared bool
-    // { _: [], version: 'help' }
-    // address after hooking up runner, cmd, user and app configs
-    t.equals(main(['--version', 'help'], noop), 'version');
-    t.end();
+    cli.log.once('log.error', function(m) {
+        t.equal(m.prefix, 'Missing command parameter.', 'err msg is wrong');
+        process.nextTick(function() {
+            t.equal(m, cli.log.record.pop(), 'err msg obj is same');
+            t.equal(0, cli.log.record.length, 'log stack empty');
+        });
+
+    });
+
+    t.equals(cli.run([], noop), 'help');
 });
 
 test('help', function(t) {
@@ -30,23 +29,54 @@ test('help', function(t) {
         t.equals(err, null);
     }
 
-    t.equals(main([], cb), 'help');
-    t.equals(main(['--help'], cb), 'help');
-    t.equals(main(['help'], cb), 'help');
-    t.equals(main(['--foo', 'help'], cb), 'help');
+    t.equals(cli.run([], cb), 'help', 'missing command');
+    t.equals(cli.run(['--help'], cb), 'help');
+    t.equals(cli.run(['help'], cb), 'help');
+    t.equals(cli.run(['--foo', 'help'], cb), 'help');
 });
 
-test('info', {skip:true}, function(t) {
+test('version', function(t) {
+    t.plan(6);
+
+    function cb(err, msg) {
+        t.equals(err, null);
+        t.ok(msg.match(/^mojito-cli v(\d+\.){2}.+/));
+    }
+
+    t.equals(cli.run(['version'], cb), 'version');
+    t.equals(cli.run(['--version'], cb), 'version');
+});
+
+test('fixme --version not treated as a bool flag', function(t) {
+    // unexpected result since --version is not declared bool
+    // { _: [], version: 'help' }
+    // address after hooking up runner, cmd, user and app configs
+    t.equals(cli.run(['--version', 'help'], noop), 'version');
+    t.end();
+});
+
+
+test('create', function(t) {
+    t.equals(cli.run(['create', 'app/simple'], noop), 'create');
+    t.end();
+});
+
+test('SKIP', {skip:true}, function(t) {
     t.plan(2);
 
     function cb(err, msg) {
         t.equals(1, 2);
     }
 
-    t.equals(main(['nonesuch'], cb), 'nonesuch');
+    t.equals(cli.run(['nonesuch'], cb), 'nonesuch');
 });
 
-test('create', function(t) {
-    t.equals(main(['create', 'app/simple'], noop), 'create');
+test('-cleanup-', function(t) {
+    //destroy accumulated log messages
+    t.ok(cli.log.record.length);
+    t.ok(cli.log._buffer.length);
+    cli.log.record = [];
+    cli.log._buffer = [];
+    cli.log.resume();
     t.end();
 });
