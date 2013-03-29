776afcd2da1469265dba2c58160063b73c2f4188
Isao Yagi <isao@yahoo-inc.com>
2013-03-29 16:28:26 -0700

    restore tests

--- tests/exports.js
+++ tests/exports.js
@@ -1,8 +1,17 @@
-var test = require('tape');
+var test = require('tap').test;
 
 
 test('index exports', function (t) {
     var module = require('../');
+    t.equal('function', typeof module);
     t.same(Object.keys(module), ['log', 'load', 'getmeta']);
     t.end();
 });
+
+test('readpkg exports', function (t) {
+    var module = require('../lib/readpkg');
+    t.equal('function', typeof module);
+    t.same(Object.keys(module), ['mojito']);
+    t.end();
+});
+
