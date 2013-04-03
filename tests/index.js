968ddca34c1841e79bb2076dcdeab2e385f3ca92
Isao Yagi <isao@yahoo-inc.com>
2013-04-03 14:44:52 -0700

    minor

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,18 @@
-require('./logtest');
-require('./readpkg');
-require('./help');
-require('./info');
-require('./main');
-require('./version');
+//require all .js files in __dirname
+
+var fs = require('fs'),
+    resolve = require('path').resolve;
+
+function load(name) {
+    var path;
+    if(name.match(/[.]js$/)) {
+        path = resolve(__dirname, name);
+        if(path !== __filename) {
+            require(path);
+        }
+    }
+}
+
+fs.readdir(__dirname, function onreaddir(err, list) {
+    list.forEach(load);
+});
