c3081f873ea808eca02164a3cccb35205b18646b
Isao Yagi <isao@yahoo-inc.com>
2013-04-02 17:26:03 -0700

    bring up to 100% coverage

--- tests/main.js
+++ tests/main.js
@@ -1,16 +1,14 @@
 var test = require('tap').test,
-    cli = require('../');
+    cli = require('../'),
+    log = cli.log;
 
 
 // buffer log msgs instead of outputing them
-cli.log.pause();
+log.pause();
 
 function reset(t) {
-    if(t) {
-        t.same(cli.log.record, cli.log._buffer, 'records & buffer are same');
-        t.ok(cli.log.record.length, 'have records buffered');
-    }
-    cli.log.record = cli.log._buffer = [];
+    log.record = [];
+    log._buffer = [];
 }
 
 function noop() {
@@ -30,19 +28,19 @@ test('index exports', function (t) {
 });
 
 test('mojito (no subcmd)', function(t) {
-    t.plan(7);
-    reset();
+    t.plan(5);
 
     function cb2(m) {
         t.equal(m.message, 'No command...', 'err msg emitted');
         process.nextTick(function() {
-            t.equal(cli.log.record[0], m, 'err msg obj is 1st elem');
-            t.ok(cli.log.record.length > 1, 'plus some other msgs');
-            reset(t);
+            t.equal(log.record[0], m, 'err msg obj is 1st elem');
+            t.ok(log.record.length > 1, 'plus some other msgs');
+            reset();
         });
     }
 
-    cli.log.once('log.error', cb2);
+    log.once('log.error', cb2);
+    reset();
     t.equals(cli([], '', noCb(t)), 'help');
 });
 
@@ -54,21 +52,42 @@ test('mojito help|--help|-h', function(t) {
 });
 
 test('mojito help (app cwd)', function(t) {
-    t.plan(3)
-
-    var cwd = __dirname + '/fixtures/someapp',
-        msgs = [];
-
-    cli.log.on('log.info', function (m) {
-        msgs.push(m.message);
-    });
+    var cwd = __dirname + '/fixtures/someapp';
 
     function cb(err, msg) {
         t.equal(err, null);
         t.equal(msg, 'mock usage for mojito/lib/app/commands/jslint.js');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.shift().level, 'debug');
+        t.equal(log.record.length, 0);
+        reset();
+        t.end();
+    }
+    reset();
+    t.equals(cli(['help', 'jslint'], cwd, cb), 'help');
+});
+
+test('exec legacy cmd', function(t) {
+    var cwd = __dirname + '/fixtures/someapp';
+
+    function cb(err, args, opts) {
+        t.equals(err, null);
+        t.same(args, []);
+        t.end();
     }
 
-    t.equals(cli(['help', 'jslint'], cwd, cb), 'help');
+    reset();
+    cli(['jslint'], cwd, cb);
+});
+
+test('--debug', function(t) {
+    t.equals(cli(['--debug', 'help'], '', noop), 'help');
+    t.equals(cli(['help', '-d'], '', noop), 'help');
+    t.end();
 });
 
 test('mojito --version', function(t) {
@@ -133,12 +152,6 @@ test('command takes priority over atl cmd flag', function(t) {
     t.end();
 });
 
-
-// test('create', function(t) {
-//     t.equals(cli(['create', 'app/simple', 'foo'], '', noop), 'create');
-//     t.end();
-// });
-
 test('nonesuch', function(t) {
     t.plan(2);
 
@@ -147,4 +160,18 @@ test('nonesuch', function(t) {
     }
 
     t.equals(cli(['nonesuch'], '', cb), 'nonesuch');
+});
+
+test('load "version"', function(t) {
+    var actual = cli.load('./commands/version');
+    t.equals(typeof actual, 'function');
+    t.equals(typeof actual.usage, 'string');
+    t.ok(actual.usage.match(/^Usage: mojito version /));
+    t.end();
+});
+
+test('load fail', function(t) {
+    var actual = cli.load('foo');
+    t.equals(actual, undefined);
+    t.end();
 });
